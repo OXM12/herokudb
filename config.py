d782eb9bd8986c353c0a644440cf7e66d8c84e0d
Ozodbek <johndoe@example.com>
2022-07-26 19:29:17 +0500

    database

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-BOT_TOKEN = "5324018241:AAHOF-y9Wd4GYv_1__pAm7CrNhXzhABCKlI"
+BOT_TOKEN = "5529145749:AAE5YygHLTQhDELBYc-deHlxFwnO6d8zlS0"
 APP_URL = "https://detectivebd.herokuapp.com/" + BOT_TOKEN
-DB_URI = "postgres://trzsoxutnkqlfh:b473f00866a5c1a61d0f63b07b7d21ee5b277e7e61e5d203bac0f1e5e7467e10@ec2-54-228-218-84.eu-west-1.compute.amazonaws.com:5432/d5pt8elm0qjn29"
+DB_URI = "postgres://mivdpmtervdhqk:a01c5af267a092514c8769bedc40fbe41468db350d22fffea4e953eef318e28f@ec2-52-30-75-37.eu-west-1.compute.amazonaws.com:5432/d5c7eqbd6vbk4b"
